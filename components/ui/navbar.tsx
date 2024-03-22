c5a6855ba580cdfcf68cad8502f4a061a33cb1b1
unknown <harmelkaur@gmail.com>
2024-03-22 17:12:36 +0530

    add clerk theming

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,13 +1,22 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
 import Logo from "./app-logo";
 import ThemeSwitcher from "./theme-switcher";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function NavBar() {
+  const { theme } = useTheme();
   return (
     <nav className="flex w-full items-center justify-between p-4 px-8 h-16">
       <Logo />
       <div className="flex items-center gap-4">
-        <UserButton afterSignOutUrl="/" />
+        <UserButton
+          afterSignOutUrl="/sign-in"
+          appearance={{
+            baseTheme: theme === "dark" ? dark : undefined,
+          }}
+        />
         <ThemeSwitcher />
       </div>
     </nav>
